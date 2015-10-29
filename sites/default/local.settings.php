ad804b565f036d77cd27889bb557dc4910504da6
Ricardo Arteta <ricardo.arteta@globant.com>
2015-10-28 20:00:27 -0500

    Working Example Profile

---
+++ sites/default/local.settings.php
@@ -0,0 +1,17 @@
+<?php
+
+$databases = array (
+  'default' =>
+  array (
+    'default' =>
+    array (
+      'database' => 'dc_example',
+      'username' => 'root',
+      'password' => 'arteta',
+      'host' => 'localhost',
+      'port' => '',
+      'driver' => 'mysql',
+      'prefix' => '',
+    ),
+  ),
+);
